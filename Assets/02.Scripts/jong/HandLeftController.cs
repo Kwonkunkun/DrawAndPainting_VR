b25c0f9948a7959d91289006509d2fbadac71f95
zigoom <rlawhdgns72@naver.com>
2020-09-19 13:03:22 +0900

    2020/09/19 작업내역
    VR 버튼 이벤트 및 기능 생성

---
+++ Assets/02.Scripts/jong/HandLeftController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+
+public class HandLeftController : MonoBehaviour
+{
+	public SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
+
+	public SteamVR_Action_Boolean trigger = SteamVR_Actions.default_InteractUI;
+    
+    public GameObject player;
+
+    bool touchBtn = false;
+    string touchName = "";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 왼쪽 버튼의 트리거 + 버튼에 컨트롤러를 가져다 댈떄
+        if(trigger.GetStateDown(leftHand) && touchBtn)
+        {
+            Debug.Log("왼쪽 트리거 누름");
+
+            if ("BtnRecog" == touchName)  // 인식버튼 
+            {
+                player.GetComponent<ButtonEvent>().BtnImgCheck();
+            }
+
+            if ("BtnEnd" == touchName)   // 종료버튼 
+            {
+                player.GetComponent<ButtonEvent>().BtnEndPy();
+
+            }
+
+            // 버튼에 있는 OnClick() 실행
+            // if(button!=null){ button.onClick.Invoke(); }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.tag);
+
+        if(other.gameObject.tag=="Button"){
+            touchBtn = true;
+            touchName = other.gameObject.name;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag=="Button"){
+            touchBtn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag=="Button"){
+            touchBtn = false;
+            touchName = "";
+        }
+    }
+
+}
---
+++ Assets/02.Scripts/jong/MyAgent.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MLAgents;
+
+public class MyAgent : Agent
+{
+    [HideInInspector]   // 파이썬 종료
+    public bool isEndPy =true;
+
+    [HideInInspector]   // 이미지 체크
+    public bool isImgCheck =false;
+    
+    private void Start()  { }
+
+
+    // 초기화
+    public override void InitializeAgent()
+    {
+        //academy = FindObjectOfType(typeof(TestAcademy)) as TestAcademy;
+    }
+
+    // 파이썬에 보낼 값을 설정(수치 추가)
+    public override void CollectObservations()
+    {
+        AddVectorObs(isEndPy);
+        AddVectorObs(isImgCheck);
+    }
+
+    int cnt = 0;
+    int preAction =0;
+
+    //파이썬의 머신러닝에 대한 액션을 입력받아서 어떻게 행동할지 해서 리워드로 다시 파이썬에 보냄
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        int action = (int)vectorAction[0];
+
+        if (action != 0) print(action.ToString());
+
+        if(preAction!=action){
+            // switch (action) {
+            //     case 0: {   // 아직 그림 인식을 안하는 상태
+
+            //             break;
+            //         }
+            //     case 1: {
+            //             playerButton.PreCreate01();
+
+            //             // 다시 그림 인식을 안하는 상태로 변경
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 2: {
+            //             playerButton.PreCreate02();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 3: {
+            //             playerButton.PreCreate03();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 4: {
+            //             playerButton.PreCreate04();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 5: {
+            //             playerButton.PreCreate05();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 6: {
+            //             playerButton.PreCreate06();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 7: {
+            //             playerButton.PreCreate07();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 8: {
+            //             playerButton.PreCreate08();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 9: {
+            //             playerButton.PreCreate09();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //     case 10: {
+            //             playerButton.PreCreate10();
+
+            //             isImgCheck = false;
+            //             break;
+            //         }
+            //}
+            //Instantiate(gb);
+            isImgCheck = false;
+            preAction=action;
+        }
+    }
+
+    // 다시 시작
+    public override void AgentReset()
+    {
+        
+    }
+
+
+}
