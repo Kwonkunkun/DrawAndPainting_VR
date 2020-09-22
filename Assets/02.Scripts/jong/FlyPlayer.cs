33943f2716e2b47d0849370e9f1fd2885b70ca02
zigoom <rlawhdgns72@naver.com>
2020-09-22 19:07:21 +0900

    2020/09/22 오후 작업내역
    시작화면 작업중

---
+++ Assets/02.Scripts/jong/FlyPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("time",10.0f);
+        ht.Add("path",iTweenPath.GetPath("Fly"));
+        ht.Add("easetype",iTween.EaseType.linear);
+
+        iTween.MoveTo(this.gameObject,ht);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
