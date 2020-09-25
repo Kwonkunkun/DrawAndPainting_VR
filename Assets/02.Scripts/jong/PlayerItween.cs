4c5ac8bab7bef3e11ee0c18d60498e7b44770edc
zigoom <rlawhdgns72@naver.com>
2020-09-25 13:33:53 +0900

    2020/09/24 오후 작업내역

---
+++ Assets/02.Scripts/jong/PlayerItween.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItween : MonoBehaviour
+{
+    //public GameObject target;
+    
+    public GameObject menu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("time",15.0f);
+        ht.Add("path",iTweenPath.GetPath("GameOpen"));
+        ht.Add("easetype",iTween.EaseType.linear);
+        ht.Add("orienttopath",true);
+        ht.Add("oncomplete", "Rotate_01");
+       // ht.Add("oncompletetarget", gameObject);
+
+        iTween.MoveTo(this.gameObject,ht);
+    }
+
+    void Rotate_01()   // 시작메뉴에서  iTween이 끝나고 92도 맟추기
+    {
+	    iTween.RotateTo(gameObject, new Vector3(0, 92, 0), 6.0f);
+    }
+    public void StartGame(){ // 게임시작 버튼을 누를 시에 실행
+
+        Hashtable ht = new Hashtable();
+        ht.Add("time",15.0f);
+        ht.Add("path",iTweenPath.GetPath("GameStart"));
+        ht.Add("easetype",iTween.EaseType.linear);
+        ht.Add("orienttopath",true);
+        ht.Add("oncomplete", "Rotate_02");
+       // ht.Add("oncompletetarget", gameObject);
+
+        iTween.MoveTo(this.gameObject,ht);
+    }
+    void Rotate_02()   // 시작게임에서 그림판 앞으로  iTween이 끝나고 270도 맟추기
+    {
+	    iTween.RotateTo(gameObject, new Vector3(0, 270, 0), 6.0f);
+    }
+
+    public void ReturnMenu(){ // 게임시작 버튼을 누를 시에 실행
+
+        Hashtable ht = new Hashtable();
+        ht.Add("time",15.0f);
+        ht.Add("path",iTweenPath.GetPath("ReturnMenu"));
+        ht.Add("easetype",iTween.EaseType.linear);
+        ht.Add("orienttopath",true);
+        ht.Add("oncomplete", "Rotate_03");
+       // ht.Add("oncompletetarget", gameObject);
+
+        iTween.MoveTo(this.gameObject,ht);
+    }
+    void Rotate_03()   // 시작게임에서 그림판 앞으로  iTween이 끝나고 270도 맟추기
+    {
+	    iTween.RotateTo(gameObject, new Vector3(0, 92, 0), 6.0f); 
+
+        menu.SetActive(true);
+    }
+}
