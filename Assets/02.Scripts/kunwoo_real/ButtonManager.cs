83535b52cf3734edd2cefea1867ee06dd6323d1b
zigoom <rlawhdgns72@naver.com>
2020-10-23 12:15:46 +0900

    2020/10/23 작업 내역
     - Scene_jong_Game 씬에 프린터 출력기능 제작

--- Assets/02.Scripts/kunwoo_real/ButtonManager.cs
+++ Assets/02.Scripts/kunwoo_real/ButtonManager.cs
@@ -9,17 +9,17 @@ public class ButtonManager : MonoBehaviour
     [SerializeField]
     MyAgent myAgent;
 
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject Temp;        //회전 테이블 자식의 위치를 이용하기 위함
+    [SerializeField]
+    private GameObject ColorTable;  //회전 테이블 객체
 
-    void Update()
-    {
-        
-    }
-    public void CheckImageButton()
-    {
+
+    void Start() { }
+
+    void Update() { }
+
+    public void ClickImageButton() {
         Debug.Log("이미지 인식버튼");
         myAgent.isImgCheck = true;
 
@@ -27,11 +27,24 @@ public class ButtonManager : MonoBehaviour
         myAgent.AgentAction(testVal,"Test");
     }
 
-    public void CheckEndButton()
-    {
+    public void ClickCheckEndButton() {
         Debug.Log("종료버튼");
         myAgent.isEndPy = false;
     }
+
+    public void ClickOutObject() {
+        myAgent.tabletDontTouch.SetActive(false);
+        myAgent.printButton.gameObject.SetActive(false);
+        myAgent.printingText.SetActive(false);
+        myAgent.GaugeImage.fillAmount = 0f;
+
+        /*  출력물을 이제 어디에 위치해야 하는지 지정  */
+        myAgent.printObject.transform.position = Temp.transform.position;
+        myAgent.printObject.transform.parent = ColorTable.transform;
+
+
+    }
+
 
     public void canvasClear(){
         StartCoroutine(canvasClearCo());
--- Assets/02.Scripts/kunwoo_real/MyAgent.cs
+++ Assets/02.Scripts/kunwoo_real/MyAgent.cs
@@ -3,24 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using MLAgents;
+using TMPro;
 
 public class MyAgent : Agent
 {
     // 파이썬 종료, 이미지 처리 여부
+
+    /// <summary>
+    /// isEndPy         : 파이썬 종료 확인
+    /// isImgCheck      : 이미지 체크 확인
+    /// modelstartPos   : 프린트 위치
+    /// 
+    /// DrawObjects             : 정규화된 모델 배열
+    /// DrawObjectsShader       : 정규화된 모델의 홀로그램 쉐이터 적용 배열
+    /// printObject             : 출력 모델 객체 
+    /// printObjectShaderApply  : 출력 모델 
+    /// 
+    /// tabletDontTouch     : 출력중 테블릿 터치방지
+    /// printingText        : 프린터에 출력중 텍스트 표시
+    /// printButton         : 프린터 꺼내기 버튼
+    /// GaugeImage          : 출력 게이지 이미지
+    /// </summary>
     public bool isEndPy = true;
     public bool isImgCheck = false;
 
-    /// <summary>
-    /// modelstartPos : 프린트 시작 위치
-    /// modelEndPos : 프린트 끝나는 위치
-    /// </summary>
     [SerializeField]
     private GameObject modelStartPos;
-    [SerializeField]
-    private GameObject modelEndPos;
 
     public List<GameObject> DrawObjects;
+    public List<GameObject> DrawObjectsShaderApply;
+    public GameObject printObject;
+    private GameObject printObjectShaderApply;
 
+    public GameObject tabletDontTouch;
+    public GameObject printingText;
+    public Button printButton;
+    public Image GaugeImage;
+
+
+    int preAction = 0;
 
     #region User define function
     public void EndPython()
@@ -35,24 +56,22 @@ public class MyAgent : Agent
     #endregion
 
     #region ML function
-    // 초기화
     public override void InitializeAgent()
     {
         //academy = FindObjectOfType(typeof(TestAcademy)) as TestAcademy;
     }
 
-    // 파이썬에 보낼 값을 설정(수치 추가)
     public override void CollectObservations()
     {
         AddVectorObs(isEndPy);
         AddVectorObs(isImgCheck);
     }
 
-    int preAction = 0;
-
-
     /// <summary>
-    /// 파이썬의 머신러닝에 대한 액션을 입력받아서 어떻게 행동할지 해서 리워드로 다시 파이썬에 보냄
+    /// InitializeAgent()       : 초기화
+    /// CollectObservations()   : 파이썬에 보낼 값을 설정(수치 추가)
+    /// AgentAction()   : 파이썬의 머신러닝에 대한 액션을 입력받아서 어떻게 행동할지 해서 리워드로 다시 파이썬에 보냄
+    /// AgentReset()    : 한회전 돌다시 시작
     /// </summary>
     public override void AgentAction(float[] vectorAction, string textAction)
     {
@@ -61,7 +80,11 @@ public class MyAgent : Agent
             return;
         } else {
             //인식이 되었을때 오브젝트 생성
-            GameObject obj = Instantiate(DrawObjects[action-1], modelStartPos.transform);
+            printObjectShaderApply = Instantiate(DrawObjectsShaderApply[action-1], modelStartPos.transform);
+            printObject = Instantiate(DrawObjects[action-1], modelStartPos.transform);
+            printObject.SetActive(false);
+            StartCoroutine(PrintObjectCo());
+
             isImgCheck = false;
             action = 0;   
         }
@@ -69,11 +92,25 @@ public class MyAgent : Agent
         //obj를 형이 paint 매니저 (싱글톤 추천) 를 만들어서 넣으면됨 
     }
 
-    // 다시 시작
-    public override void AgentReset()
-    {
-        
-    }
+    public override void AgentReset() { }
+    #endregion
 
+    #region 프린터 출력을 위한 코루틴 생성
+    IEnumerator PrintObjectCo()
+    {
+        tabletDontTouch.SetActive(true);
+        printingText.SetActive(true);
+
+        for (int i = 0; i < 5; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            GaugeImage.fillAmount += 0.2f;
+        }
+        Destroy(printObjectShaderApply); 
+
+        printingText.SetActive(false);
+        printObject.SetActive(true);
+        printButton.gameObject.SetActive(true);
+    }
     #endregion
 }
